063f83436246960ff950c9156528be2aab9dd59b
ZavodVenture <127696238+ZavodVenture@users.noreply.github.com>
2023-09-08 20:28:40 +0300

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,43 @@
+import { HardhatUserConfig } from "hardhat/config";
+
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 
-module.exports = {
+import "@matterlabs/hardhat-zksync-verify";
+
+// dynamically changes endpoints for local tests
+const zkSyncTestnet =
+  process.env.NODE_ENV == "test"
+    ? {
+        url: "http://localhost:3050",
+        ethNetwork: "http://localhost:8545",
+        zksync: true,
+      }
+    : {
+        url: "https://zksync2-testnet.zksync.dev",
+        ethNetwork: "goerli",
+        zksync: true,
+        // contract verification endpoint
+        verifyURL:
+          "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+      };
+
+const config: HardhatUserConfig = {
   zksolc: {
-    version: "1.3.13",
+    version: "1.13.3",
     compilerSource: "binary",
     settings: {},
   },
   defaultNetwork: "zkSyncTestnet",
-
   networks: {
-    zkSyncTestnet: {
-      url: "https://testnet.era.zksync.dev",
-      ethNetwork: "goerli", // RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-      zksync: true,
+    hardhat: {
+      zksync: false,
     },
+    zkSyncTestnet,
   },
   solidity: {
-    version: "0.8.8",
+    version: "0.8.17",
   },
 };
+
+export default config;
