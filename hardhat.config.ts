f39033e3edfd6286a9db5af5b639063c9f0e6b7d
ZavodVenture <127696238+ZavodVenture@users.noreply.github.com>
2023-12-19 01:28:16 +0300

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -41,7 +41,7 @@ const config: HardhatUserConfig = {
     },
   },
   zksolc: {
-    version: "1.3.19",
+    version: "1.3.17",
     settings: {
       // find all available options in the official documentation
       // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
